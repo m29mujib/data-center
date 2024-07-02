7f953f2fa7a80ec975ffc9e61b253886c833cd2d
m29mujib <mamujib29@gmail.com>
2024-07-02 17:05:45 +0700

    mengubah judul about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,37 @@
 const About = () => {
-    return(
+  return (
     <div className="w-full h-screen flex  items-center justify-between px-[8%]">
-        <div className="">
-            <h1 className="text-5xl font-semibold mb-5 max-w-[600px] font-customFont text-[#333]">Apartmen</h1>
-            <p className="text-sm max-w-[500px] font-customFont text-[#333]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptatum deleniti expedita quaerat necessitatibus libero?</p>
-            <p className="text-sm max-w-[500px] font-customFont text-[#333]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, esse.</p>
-            <button className="py-2 px-5 bg-white text-gray-600 rounded-sm mt-10 mr-5 font-customFont text-[#333]">Iklan disini</button>
-            <button className="py-2 px-5 bg-blue-600 text-white rounded-sm mt-10 font-customFont">Read More</button>
+      <div className="">
+        <h1 className="text-5xl font-semibold mb-5 max-w-[500px] font-customFont text-[#333]">
+          Search & Rent Apartment Easily
+        </h1>
+        <p className="text-sm max-w-[500px] font-customFont text-[#333]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          voluptatum deleniti expedita quaerat necessitatibus libero?
+        </p>
+        <p className="text-sm max-w-[500px] font-customFont text-[#333]">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus,
+          esse.
+        </p>
+        <button className="py-2 px-5 bg-white text-gray-600 rounded-sm mt-10 mr-5 font-customFont text-[#333]">
+          Iklan disini
+        </button>
+        <button className="py-2 px-5 bg-blue-600 text-white rounded-sm mt-10 font-customFont">
+          Read More
+        </button>
+      </div>
+      <div className="flex">
+        <div className="flex justify-between">
+          <div className="w-[350px] h-[350px] rounded-full bg-blue-600 relative border">
+            <img
+              src="../src/assets/hp.png"
+              alt=""
+              className="absolute top-[-0px] left-[-0px] w-[500px] h-[500px]"
+            />
+          </div>
         </div>
-        <div className="flex">
-            <div className="flex justify-between">
-                <div className="w-[350px] h-[350px] rounded-full bg-blue-600 relative border">
-                    <img src="../src/assets/hp.png" alt="" className="absolute top-[-0px] left-[-0px] w-[500px] h-[500px]"/>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-    )
-}
-export default About
+  );
+};
+export default About;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,44 @@
 const Navbar = () => {
-    return (
+  return (
     <div className="w-full h-[50px] bg-white py-10 px-[8%] flex justify-between items-center fixed top-0 left-0 z-10">
-        <div className="flex">
-            <img src="../../assets/a.png" alt="" class="w-[30px] mr-[8px]"/>
-            <h1 className=" font-semibold tracking-wide font-customFont text-[#333]">Apartment</h1>
-        </div>
-        <div className="flex items-center justify-right">
-            <ul>
-                <li>
-                    <a href="/home" className="decoration-0 px-5 font-semibold font-customFont text-[#333]">Home</a>
-                    <a href="/home" className="decoration-0 px-5 font-semibold font-customFont text-[#333]">About Us</a>
-                    <a href="/home" className="decoration-0 px-5 font-semibold font-customFont text-[#333]">Blog</a>
-                    <a href="/home" className="decoration-0 px-5 text-white bg-blue-500 font-semibold font-customFont py-3 rounded-sm">Get Contact</a>
-                </li>
-            </ul>
-        </div>
+      <div className="flex">
+        <img src="../src/assets/a.png" alt="" class="w-[30px] mr-[8px]" />
+        <h1 className=" font-semibold tracking-wide font-customFont text-[#333]">
+          Apartment
+        </h1>
+      </div>
+      <div className="flex items-center justify-right">
+        <ul>
+          <li>
+            <a
+              href="/home"
+              className="decoration-0 px-5 font-semibold font-customFont text-[#333]"
+            >
+              Home
+            </a>
+            <a
+              href="/home"
+              className="decoration-0 px-5 font-semibold font-customFont text-[#333]"
+            >
+              About Us
+            </a>
+            <a
+              href="/home"
+              className="decoration-0 px-5 font-semibold font-customFont text-[#333]"
+            >
+              Blog
+            </a>
+            <a
+              href="/home"
+              className="decoration-0 px-5 text-white bg-blue-500 font-semibold font-customFont py-3 rounded-sm"
+            >
+              Get Contact
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
