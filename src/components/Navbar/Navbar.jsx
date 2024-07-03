ce82d71d2becff1c99db412d5799ea64123f7ecc
ilhamfakihhilmi <ilhamfakih50@gmail.com>
2024-07-03 10:37:56 +0700

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,30 @@
 const Navbar = () => {
   return (
-    <div className="w-full h-[50px] bg-white py-10 px-[8%] flex justify-between items-center fixed top-0 left-0 z-10">
+    <div className="w-full h-[50px] bg-black bg-opacity-10 backdrop-blur-lg py-10 px-[8%] flex justify-between items-center fixed top-0 left-0 z-10">
       <div className="flex">
-        <img src="../src/assets/a.png" alt="" class="w-[30px] mr-[8px]" />
-        <h1 className=" font-semibold tracking-wide font-customFont text-[#333]">
-          Apartment
+        <img src="../src/assets/a.png" alt="" className="w-[30px] mr-[8px]" />
+        <h1 className="font-semibold tracking-wide font-customFont text-[#ffffff]">
+          Data Center
         </h1>
       </div>
       <div className="flex items-center justify-right">
-        <ul class="hidden md:block">
+        <ul>
           <li>
             <a
               href="/home"
-              className="decoration-0 px-5 font-semibold font-customFont text-[#333]"
+              className="decoration-0 px-5 font-regular font-customFont text-[#ffffff]"
             >
               Home
             </a>
             <a
               href="/home"
-              className="decoration-0 px-5 font-semibold font-customFont text-[#333]"
+              className="decoration-0 px-5 font-regular font-customFont text-[#ffffff]"
             >
               About Us
             </a>
             <a
               href="/home"
-              className="decoration-0 px-5 font-semibold font-customFont text-[#333]"
+              className="decoration-0 px-5 font-regular font-customFont text-[#ffffff]"
             >
               Blog
             </a>
