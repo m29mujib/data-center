343f1227cb5ce13081b857f7801818091d5bb43c
ilhamfakihhilmi <ilhamfakih50@gmail.com>
2024-07-04 23:23:16 +0700

    update navbar

--- src/components/Iklan/Iklan.jsx
+++ src/components/Iklan/Iklan.jsx
@@ -8,25 +8,24 @@ const Iklan = () => {
     scrollSpyDelay: 1000,
   });
   return (
-    <div className="flex flex-col px-20 py-[3%]">
-      <div className="atas">
-        <p className="max-w-[500px] text-sm text-[#333] font-customFont"></p>
-      </div>
-      <div className="bawah mt-5 flex flex-wrap justify-evenly items-center bg-white shadow-md rounded-lg">
-        <div className="h-full border-r border-gray-300 pr-5">
-          <div className="flex flex-col md:flex-row items-center rounded-lg">
-            <div className="App">
-              <div className="content flex justify-center items-center"></div>
-              <CountUp enableScrollSpy className="opacity-0" />
-              <br />
-              <div className="flex">
-                <span
-                  id="counter"
-                  className="text-[#00B5FF] text-4xl font-customFont font-bold"
-                ></span>
-                <p className="text-[#00B5FF] text-4xl font-customFont font-bold">
-                  K
-                </p>
+    <div className="flex  flex-col px-20 py-[3%]">
+      <div className="bawah mt-5 flex flex-wrap justify-evenly items-center  bg-white shadow-md rounded-lg">
+        <div className="h-full " style={{ borderRight: "2px solid #808080" }}>
+          <div className="flex flex-col md:flex-row items-center  rounded-lg">
+            <div className="w-20">
+              <div className="App">
+                <div className="content flex justify-center items-center"></div>
+                <CountUp enableScrollSpy className="opacity-0" />
+                <br />
+                <div className="flex">
+                  <span
+                    id="counter"
+                    className="text-[#00B5FF] text-4xl font-customFont font-bold"
+                  ></span>
+                  <p className="text-[#00B5FF] text-4xl font-customFont font-bold">
+                    K
+                  </p>
+                </div>
               </div>
             </div>
             <p className="md:text-xl pr-10 pt-6 md:ml-5 text-sm text-gray-600 font-semibold font-customFont text-[#333]">
@@ -38,93 +37,107 @@ const Iklan = () => {
           </p>
         </div>
 
-        <div className="flex flex-col border-r border-gray-300 pr-5">
-          <div className="flex flex-col md:flex-row items-center rounded-lg">
-            <div className="App">
-              <div className="content"></div>
-              <p className="opacity-0">0</p>
-              <CountUp
-                end={2500}
-                enableScrollSpy
-                className="text-[#00B5FF] text-4xl font-customFont font-bold"
-              />
-              <br />
-              <span
-                id="counter"
-                className="text-[#333] text-xl font-customFont font-bold"
-              ></span>
+        <div
+          className="flex flex-col pr-20"
+          style={{ borderRight: "2px solid #808080" }}
+        >
+          <div className="flex flex-col md:flex-row items-center  rounded-lg">
+            <div className="w-20">
+              <div className="App">
+                <div className="content"></div>
+                <p className="opacity-0">0</p>
+                <CountUp
+                  end={2500}
+                  enableScrollSpy
+                  className="text-[#00B5FF] text-4xl font-customFont font-bold"
+                />
+                <br />
+                <span
+                  id="counter"
+                  className="text-[#333] text-xl font-customFont font-bold"
+                ></span>
+              </div>
             </div>
-            <p className="md:text-xl pt-6 md:ml-5 text-sm text-gray-600 font-semibold font-customFont text-[#333]">
+            <p className="md:text-xl pt-6 md:ml-8 text-sm text-gray-600 font-semibold font-customFont text-[#333]">
               POP National
             </p>
           </div>
-          <p className="md:text-sm text-sm text-gray-600 font-regular font-customFont text-[#333]">
+          <p className="md:text-sm   text-sm text-gray-600 font-regular font-customFont text-[#333]">
             Coverage
           </p>
         </div>
 
-        <div className="flex flex-col md:flex-row items-center pb-5 pl-0 rounded-lg border-r border-gray-300 pr-5">
-          <div className="col">
-            <div className="App">
-              <div className="content"></div>
-              <p className="opacity-0">0</p>
-              <CountUp
-                end={10}
-                enableScrollSpy
-                className="text-[#00B5FF] text-4xl font-customFont font-bold"
-              />
-              <br />
-              <span
-                id="counter"
-                className="text-[#333] text-xl font-customFont font-bold"
-              ></span>
-            </div>
-            <div className="App">
-              <div className="content"></div>
-              <CountUp
-                end={27}
-                enableScrollSpy
-                className="text-[#00B5FF] text-4xl font-customFont font-bold"
-              />
-              <br />
-              <span
-                id="counter"
-                className="text-[#333] text-xl font-customFont font-bold pb-10"
-              ></span>
+        <div className="flex flex-col md:flex-row items-center  pb-5  rounded-lg">
+          <div className="pr-2 w-10">
+            <div className=" pl-0">
+              <div className="App ">
+                <div className="content"></div>
+                <p className="opacity-0">0</p>
+                <CountUp
+                  end={10}
+                  enableScrollSpy
+                  className="text-[#00B5FF] text-4xl font-customFont font-bold"
+                />
+                <br />
+                <span
+                  id="counter"
+                  className="text-[#333] text-xl font-customFont font-bold"
+                ></span>
+              </div>
+
+              <div className="App ">
+                <div className="content"></div>
+                <CountUp
+                  end={27}
+                  enableScrollSpy
+                  className="text-[#00B5FF] text-4xl font-customFont font-bold"
+                />
+                <br />
+                <span
+                  id="counter"
+                  className="text-[#333] text-xl font-customFont font-bold pb-10"
+                ></span>
+              </div>
             </div>
           </div>
-          <div className="col">
-            <div className="App">
-              <p className="md:text-sm pt-9 pb-3 md:ml-3 text-sm text-gray-600 font-regular font-customFont text-[#333]">
+
+          <div
+            className=" h-20 mt-4 "
+            style={{ borderRight: "2px solid #808080", paddingRight:"20px" }}
+          >
+            <div className="App mt-6 pr-10">
+              <p className="md:text-sm  md:ml-3 text-sm text-gray-600 font-bold font-customFont text-[#333]">
                 Regional SBU
               </p>
             </div>
-            <p className="md:text-sm pt-1 md:ml-3 text-sm text-gray-600 font-regular font-customFont text-[#333]">
+            <p className="pt-4 md:text-ms pt-1 md:ml-3 text-sm text-gray-600 font-bold font-customFont text-[#333]">
               Brances Unit
             </p>
           </div>
         </div>
 
         <div>
-          <div className="flex flex-col md:flex-row items-center rounded-lg">
-            <div className="App flex">
-              <div className="content flex justify-center items-center text-[#00B5FF] text-4xl font-customFont font-bold">
-                1,
-              </div>
-              <CountUp
-                end={8}
-                enableScrollSpy
-                className="text-[#00B5FF] text-4xl font-customFont font-bold"
-              />
-              <br />
-              <div className="flex">
-                <span
-                  id="counter"
-                  className="text-[#00B5FF] text-4xl font-customFont font-bold"
-                ></span>
-                <p className="text-[#00B5FF] text-4xl font-customFont font-bold">
-                  M
-                </p>
+          <div className="flex flex-col md:flex-row items-center  rounded-lg">
+            <div className="w-20 pt-4">
+              <div className="App flex">
+                <div className="pt-1 content flex justify-center items-center text-[#00B5FF] text-4xl font-customFont font-bold">
+                  1,
+                </div>
+                <CountUp
+                  end={8}
+                  enableScrollSpy
+                  className="text-[#00B5FF] text-4xl font-customFont font-bold pt-1"
+                />
+                <br />
+                <div className="flex">
+                  <span
+                    id="counter"
+                    className="text-[#00B5FF] text-4xl font-customFont font-bold"
+                  ></span>
+                  <p className="pt-1 text-[#00B5FF] text-4xl font-customFont font-bold">
+                    M
+                  </p>
+                </div>
               </div>
             </div>
             <p className="md:text-xl pt-6 md:ml-5 text-sm text-gray-600 font-semibold font-customFont text-[#333]">
@@ -132,7 +145,7 @@ const Iklan = () => {
             </p>
           </div>
           <p className="md:text-sm text-sm text-gray-600 font-regular font-customFont text-[#333]">
-            Telco & IT Service
+            SUTT, SUTM, SUTR
           </p>
         </div>
       </div>
--- src/components/Kolaborate/Kolaborate.jsx
+++ src/components/Kolaborate/Kolaborate.jsx
@@ -1,37 +1,55 @@
 import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
+
 const Kolaborate = () => {
   const handleDragStart = (e) => e.preventDefault();
 
   const items = [
-    <img
-      src="../src/assets/comm1.jpg"
-      className=" w-[350px] h-[350px] mr-5 rounded-lg"
-      onDragStart={handleDragStart}
-      role="presentation"
-    />,
-    <img
-      src="../src/assets/comm3.jpg"
-      className=" w-[350px] h-[350px] rounded-lg"
-      onDragStart={handleDragStart}
-      role="presentation"
-    />,
-    <img
-      src="../src/assets/comm2.jpg"
-      className=" w-[350px] h-[350px] rounded-lg"
-      onDragStart={handleDragStart}
-      role="presentation"
-    />,
-    <img
-      src="../src/assets/r.jpg"
-      className="w-[350px] h-[350px] rounded-lg"
-      onDragStart={handleDragStart}
-      role="presentation"
-    />,
+    <div className="mx-[10px]" key="1">
+      <img
+        src="../src/assets/comm1.jpg"
+        className="w-[600px] h-[190px] rounded-lg"
+        onDragStart={handleDragStart}
+        role="presentation"
+      />
+    </div>,
+    <div className="mx-[10px]" key="2">
+      <img
+        src="../src/assets/comm3.jpg"
+        className="w-[600px] h-[190px] rounded-lg"
+        onDragStart={handleDragStart}
+        role="presentation"
+      />
+    </div>,
+    <div className="mx-[10px]" key="3">
+      <img
+        src="../src/assets/comm2.jpg"
+        className="w-[600px] h-[190px] rounded-lg"
+        onDragStart={handleDragStart}
+        role="presentation"
+      />
+    </div>,
+    <div className="mx-[10px]" key="4">
+      <img
+        src="../src/assets/r.jpg"
+        className="w-[600px] h-[190px] rounded-lg"
+        onDragStart={handleDragStart}
+        role="presentation"
+      />
+    </div>,
+    <div className="mx-[10px]" key="4">
+      <img
+        src="../src/assets/r.jpg"
+        className="w-[600px] h-[190px] rounded-lg"
+        onDragStart={handleDragStart}
+        role="presentation"
+      />
+    </div>,
   ];
+
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center px-[5%] py-[3%] md:px-[5%] md:py-[5%]">
+    <div className="w flex flex-col items-center justify-center px-[5%] py-[3%] md:px-[5%] md:py-[5%]">
       <div>
         <h1 className="text-4xl font-customFont text-[#333]">Kolaborasi</h1>
       </div>
@@ -41,8 +59,7 @@ const Kolaborate = () => {
           items={items}
           infinite
           autoPlay
-          autoPlayInterval={2000}
-          // infinite
+          autoPlayInterval={1000}
           responsive={{
             0: { items: 1 },
             640: { items: 2 },
