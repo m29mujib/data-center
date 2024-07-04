b566f9097b4e8bcb1c9a9e0528363c81c29f6514
ilhamfakihhilmi <ilhamfakih50@gmail.com>
2024-07-04 09:53:04 +0700

    update navbar, parallax

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,16 +8,22 @@ const Navbar = () => {
         <div className="w-[200px] h-screen bg-[#fff] absolute top-0 right-0 z-[-99] flex flex-col justify-center items-center origin-right transition-all duration-700 ease-in-out transform translate-x-0">
           <ul className="flex flex-col justify-center items-center">
             <li className="flex flex-col justify-center items-center">
-              <a href="" className="p-5">
+              <a href="" className="p-5 hover:underline hover:text-blue-500">
                 About
               </a>
-              <a href="" className="p-5">
+              <a href="" className="p-5 hover:underline hover:text-blue-500">
                 Kontak
               </a>
-              <a href="" className="p-5">
+              <a href="" className="p-5 hover:underline hover:text-blue-500">
                 Blog
               </a>
-              <a href="" className="p-5">
+              <a href="" className="p-5 hover:underline hover:text-blue-500">
+                Apartment
+              </a>
+              <a href="" className="p-5 hover:underline hover:text-blue-500">
+                Apartment
+              </a>
+              <a href="" className="p-5 hover:underline hover:text-blue-500">
                 Apartment
               </a>
             </li>
@@ -30,30 +36,46 @@ const Navbar = () => {
           Data Center
         </h1>
       </div>
+
       <div className="flex items-center justify-right">
         <ul className="hidden md:block">
           <li>
+            {/* start nav main */}
             <a
               href="/home"
-              className="decoration-0 px-5 font-regular font-customFont text-[#ffffff]"
+              className="decoration-0 px-5 font-regular font-customFont text-[#ffffff] hover:underline hover:text-blue-500"
             >
               Home
             </a>
             <a
               href="/home"
-              className="decoration-0 px-5 font-regular font-customFont text-[#ffffff]"
+              className="decoration-0 px-5 font-regular font-customFont text-[#ffffff] hover:underline hover:text-blue-500"
             >
               About Us
             </a>
             <a
               href="/home"
-              className="decoration-0 px-5 font-regular font-customFont text-[#ffffff]"
+              className="decoration-0 px-5 font-regular font-customFont text-[#ffffff] hover:underline hover:text-blue-500"
             >
               Blog
             </a>
             <a
               href="/home"
-              className="decoration-0 px-5 text-white bg-blue-500 font-semibold font-customFont py-3 rounded-sm"
+              className="decoration-0 px-5 font-regular font-customFont text-[#ffffff] hover:underline hover:text-blue-500"
+            >
+              Blog
+            </a>
+
+            <a
+              href="/home"
+              className="decoration-0 px-5 font-regular font-customFont text-[#ffffff] hover:underline hover:text-blue-500"
+            >
+              Blog
+            </a>
+            {/* end nav main */}
+            <a
+              href="/home"
+              className="decoration-0 px-5 text-white bg-blue-500 font-semibold font-customFont py-3 rounded-sm hover:underline hover:text-blue-500"
             >
               Get Contact
             </a>
